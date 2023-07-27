6a80f9ba9d1ed0edc1944586a359cfdc9cc2738d
vinay <vinaychaurasiya73@gmail.com>
2023-07-28 00:31:03 +0530

    server-allocations

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,1 +1,12 @@
-import mongoose from "mongoose"
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+}
+
+module.exports = connectDB
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('dotenv').config();
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+// app.use(helmet());
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
